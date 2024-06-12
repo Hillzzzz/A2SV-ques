cb6f25eb761c378f25e81640346607aad3c75f4e
Hillzzzz <163678865+Hillzzzz@users.noreply.github.com>
2024-06-12 19:33:54 +0300

    Create DecryptStringfromAlphabettoIntegerMapping.py

---
+++ DecryptStringfromAlphabettoIntegerMapping.py
@@ -0,0 +1,12 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        i = len(s) - 1
+        ans = ''
+        while i >= 0:
+            if s[i] == '#':
+                ans = chr(int(s[i - 2:i]) + 96) + ans
+                i -= 3
+            else:
+                ans = chr(int(s[i]) + 96) + ans
+                i -= 1
+        return ans

eda10b7280a652bf2e01a6aa4c96b943f195e629
Hillzzzz <163678865+Hillzzzz@users.noreply.github.com>
2024-07-20 21:36:51 +0300

    Update Toeplitz_matrix.py

--- Toeplitz_matrix.py
+++ Toeplitz_matrix.py
@@ -1,10 +1,9 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        m = len(matrix)
-        n = len(matrix[0])
-
-        for i in range(m - 1):
-            for j in range(n - 1):
+        numrow = len(matrix)
+        numcol = len(matrix[0])
+        for i in range(numrow - 1):
+            for j in range(numcol - 1):
                 if matrix[i][j] != matrix[i + 1][j + 1]:
                     return False
         return True
